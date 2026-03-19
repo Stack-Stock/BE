d47c47c8469162f5b1820dfa45f0cf04cf8f05e0
sk071219@naver.com <sk071219@naver.com>
2026-03-20 03:08:49 +0900

    feat: ap-consume api added

--- backend/src/main/java/com/stacknstock/backend/domain/action/enums/ActionType.java
+++ backend/src/main/java/com/stacknstock/backend/domain/action/enums/ActionType.java
@@ -1,5 +1,5 @@
 package com.stacknstock.backend.domain.action.enums;
 
 public enum ActionType {
-    INFO_PHONE, INFO_TV, INFO_PAPER, STUDY, SLEEP, EVENT
+    INFO_PHONE, INFO_TV, INFO_PAPER, STUDY, SLEEP, USE_INSPIRATION, EVENT
 }
